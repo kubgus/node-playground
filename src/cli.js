46385fae026383744f48f891eb0a15d100d4e015
Kubo <53797257+Kubicko@users.noreply.github.com>
2021-10-08 23:15:27 +0200

    VERY early cli

---
+++ src/cli.js
@@ -0,0 +1,87 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+
+
+// Take raw arguments and make them more accessible:
+function parseArgumentsIntoOptions(rawArgs) {
+
+    // Arguments:
+    const args = arg(
+        {
+
+            '--install': Boolean,
+            '-i': '--install',
+
+        },
+        {
+            argv: rawArgs.slice(2),
+        }
+    );
+
+    // Return options:
+    return {
+
+        template: args._[0] || false,
+        runInstall: args['--install'] || false,
+
+    };
+
+};
+
+
+// Make user select missing options:
+async function promptForMissingOptions(options) {
+
+
+    // Define questions:
+    const questions = [];
+
+
+    // Selection template:
+    if (!options.template) {
+
+        questions.push({
+
+            type: 'list',
+            name: 'template',
+            message: 'Please input the number of the script you want to run.',
+            choices: ['01', '02', '03', '04', '05'],
+
+        });
+
+    };
+
+
+    // Select program to run:
+    const answers = await inquirer.prompt(questions);
+    return {
+
+        ...options,
+        template: options.template || answers.template
+
+    }
+
+};
+
+export async function cli(args) {
+
+    // Make arguments readable:
+    let options = parseArgumentsIntoOptions(args);
+
+    // Make the user select missing options:
+    options = await promptForMissingOptions(options);
+
+    module.exports = (commandOptions) => {
+
+        let {
+            fileNumber,
+            callback
+        } = commandOptions
+
+        callback(commandOptions);
+
+        return;
+
+    };
+
+}
